6aaa2774f6fca8e65d4e7a0c2c99856f0ca8f6e0
巨龙之路 <648813609@qq.com>
2026-01-09 08:47:45 +0800

    add 3D particle effects for ball
    ball trail particle effect
    ball impact particle effect

---
+++ main/ball_particle.go
@@ -0,0 +1,11 @@
+embedded_components {
+  id: "model"
+  type: "model"
+  data: "mesh: \"/builtins/assets/meshes/sphere.dae\"\n"
+  "name: \"{{NAME}}\"\n"
+  "materials {\n"
+  "  name: \"default\"\n"
+  "  material: \"/scene3d/materials/basic_color.material\"\n"
+  "}\n"
+  ""
+}
